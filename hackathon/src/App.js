4a36e4b28652abe98ce020e015f519647d38e9d7
z5160345 <36937392+z5160345@users.noreply.github.com>
2019-07-27 15:26:52 +1000

    fish logo

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -1,24 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import CardMedia from '@material-ui/core/CardMedia';
+import Card from '@material-ui/core/Card'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+
 
 function App() {
+    const classes = useStyles();
+
+    const styles = {
+    media: {
+      height: "20%",
+      width: "20%",
+      paddingTop: '20%', // 16:9,
+      objectFit: "fit",
+      textAlign: "centered",
+      margin: "auto"
+    }
+
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Card>
+            <CardMedia
+              className={classes.media}
+              image={require('./pictures/fish.jpg')}
+              title="Contemplative Reptile"
+              style={styles.media}
+            />
+        </Card>
+        <TextField
+            fullWidth
+            variant="outlined"
+            label="Enter Type of Fish"
+        />
+        <Button variant="contained">
+            Search
+        </Button>
     </div>
   );
 }
